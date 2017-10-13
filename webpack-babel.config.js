19d28be6fb3db52639dee2a6f3b678f1a6ad4908
Augustin Trancart <augustin.trancart@oslandia.com>
2017-10-13 14:55:41 +0200

    Try to use define and UglifyJsPlugin with babel-plugin-webpack-loader

--- webpack-babel.config.js
+++ webpack-babel.config.js
@@ -1,10 +1,22 @@
+var webpack = require('webpack');
 var path = require('path');
+
+var definePlugin = new webpack.DefinePlugin({
+    __DEBUG__: "false", // I don't see a use case to have this true in transpiled source. Might be revisited later.
+});
 
 module.exports = {
     output: {
         libraryTarget: 'commonjs2',
         umdNamedDefine: true,
     },
+    plugins: [
+        definePlugin,
+        new webpack.optimize.UglifyJsPlugin({
+            compress: true,
+            mangle: false
+        }),
+    ],
     module: {
         rules: [
             // please consider modifying corresponding loaders in webpack.config.js too
